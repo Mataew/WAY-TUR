2b853c0a433e448e6c6176ff7d20b201f132b3ee
Umar <mister.umars@mail.ru>
2022-02-01 15:38:15 +0300

    test2

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(express.json());
 app.use("/images", express.static(path.resolve(__dirname, "images")));
 app.use(require("./routes/index"));
 
-app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+app.use(express.static(path.resolve(__dirname, 'client', 'public')))
 app.get('*', (req,res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'src', 'index.js'))
+  res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
 })
 
 const connect = async () => {
